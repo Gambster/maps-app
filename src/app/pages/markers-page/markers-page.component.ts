9d05da1b5018646280ff7248736f8f8be6506e3c
GAMBSTER <gamboa040200@gmail.com>
2025-07-06 02:31:17 -0600

    feat: implement markers page with Mapbox integration and initial map setup

--- src/app/pages/markers-page/markers-page.component.ts
+++ src/app/pages/markers-page/markers-page.component.ts
@@ -1,8 +1,46 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  signal,
+  viewChild,
+} from '@angular/core';
+import { environment } from '../../../environments/environment';
+import mapboxgl from 'mapbox-gl';
+
+mapboxgl.accessToken = environment.mapboxKey;
 
 @Component({
   selector: 'app-markers-page',
   imports: [],
   templateUrl: './markers-page.component.html',
 })
-export class MarkersPageComponent { }
+export class MarkersPageComponent implements AfterViewInit {
+  divElement = viewChild<ElementRef>('map');
+  map = signal<mapboxgl.Map | null>(null);
+
+  async ngAfterViewInit() {
+    if (!this.divElement()?.nativeElement) return;
+
+    await new Promise((resolve) => setTimeout(resolve, 80));
+
+    const element = this.divElement()!.nativeElement;
+
+    const map = new mapboxgl.Map({
+      container: element,
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: [-103.4972323, 25.5627186],
+      zoom: 14,
+    });
+
+    const market = new mapboxgl.Marker({
+      color: 'purple',
+    })
+      .setLngLat([-103.4972323, 25.5627186])
+      .addTo(map);
+
+    this.mapListeners(map);
+  }
+
+  mapListeners(map: mapboxgl.Map) {}
+}
